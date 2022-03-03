585c7170be67450d9d40182ec79c382bf7f96241
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-03 13:26:30 +0100

    Create grub-theme.sh

---
+++ grub-theme.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /a
+git clone https://github.com/gustawho/grub2-theme-breeze.git
+cd grub2-theme-breeze
+sudo cp -r breeze /usr/share/grub/themes/
+sudo sed -i 's|#GRUB_THEME=.*|GRUB_THEME="/usr/share/grub/themes/breeze/theme.txt"|' /etc/default/grub
+sudo sed -i 's|.*GRUB_GFXMODE=.*|GRUB_GFXMODE=1366x768|' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
