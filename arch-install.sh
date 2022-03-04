085f86a5d5c496a2d87398da36862e99df15a7e0
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-04 08:40:18 +0100

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -10,7 +10,7 @@ pacman -S reflector --noconfirm --needed
 reflector --country 'United Kingdom',France,Germany --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
 #essential packages
-pacman -Syyu networkmanager sudo wget curl grub efibootmgr dosfstools mtools pciutils usbutils linux-headers rsync unzip openssh --noconfirm --needed
+pacman -Syyu networkmanager sudo wget curl grub efibootmgr dosfstools mtools pciutils usbutils linux-headers rsync unzip openssh htop --noconfirm --needed
 
 #install microcode uncomment to select
 #sudo pacman -S intel-ucode --noconfirm --needed
@@ -19,16 +19,23 @@ pacman -Syyu networkmanager sudo wget curl grub efibootmgr dosfstools mtools pci
 #Enable network manager
 systemctl enable NetworkManager.service
 
+#create locales
+sed -i 's|#en_GB.UTF|en_GB.UTF|' /etc/locale.gen
+locale-gen
+
 #create files CHOOSE HOST NAME
 echo LANG=en_GB.UTF-8 >> /etc/locale.conf
 echo KEYMAP=uk >> /etc/vconsole.conf
 echo (hostname) >> /etc/hostname
 
+#change swappiness
+echo vm.swappiness=5 >> /etc/sysctl.d/99-swappiness.conf
+
 #install grub un-comment and add boot device
 
 #grub-install --target=i386-pc /dev/??
 
-#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=?????
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch-KDE
 
 
 #make grub config
