9383b29ffa31ad2428364e8801ff5dffb9e0a153
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-07 07:52:45 +0100

    Update grub-theme.sh

--- grub-theme.sh
+++ grub-theme.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-cd /a
-git clone https://github.com/gustawho/grub2-theme-breeze.git
-cd grub2-theme-breeze
+cd /a/arch-install.sh
 cp -r breeze /usr/share/grub/themes/
 sed -i 's|#GRUB_THEME=.*|GRUB_THEME="/usr/share/grub/themes/breeze/theme.txt"|' /etc/default/grub
 sed -i 's|.*GRUB_GFXMODE=.*|GRUB_GFXMODE=1366x768|' /etc/default/grub
-echo GRUB_DISABLE_OS_PROBER=false >> /etc/defalt/grub
+sed -i 's|#GRUB_DISABLE_OS_PROBER=false|GRUB_DISABLE_OS_PROBER=true|'  /etc/defalt/grub
 grub-mkconfig -o /boot/grub/grub.cfg
