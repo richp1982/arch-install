c4219849d88dc81586766e29b35e30107e3bbc99
richp1982 <100537747+richp1982@users.noreply.github.com>
2022-03-21 09:44:48 +0100

    Update bash-setup.sh

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /a
-git clone git://github.com/ohmybash/oh-my-bash.git /home/rich/.oh-my-bash
-git clone git://github.com/richp1982/dot-files-arch.git
+git clone https://github.com/ohmybash/oh-my-bash.git /home/rich/.oh-my-bash
+git clone https://github.com/richp1982/dot-files-arch.git
 cp /a/dot-files-arch/bashrc /home/rich/.bashrc
 chown rich:rich /home/rich/.bashrc
 chown -R rich:rich /home/rich/.oh-my-bash
